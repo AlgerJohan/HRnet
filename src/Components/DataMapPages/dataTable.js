23bfd0def072b2cb4ee0c8bbfec235630c18c0a9
Johan Alger <johan.alger@gmail.com>
2022-09-27 12:10:47 +0200

    retrait de beug

--- src/Components/DataMapPages/dataTable.js
+++ src/Components/DataMapPages/dataTable.js
@@ -1,83 +1,84 @@
 import React from "react";
 import DataTable from "react-data-table-component";
-import styled from "styled-components";
-import Button from "../shared/Button";
-import faker from "faker";
-const createUser = () => ({
-  id: faker.datatype.uuid(),
-  name: faker.name.findName(),
-  email: faker.internet.email(),
-  address: faker.address.streetAddress(),
-  bio: faker.lorem.sentence(),
-  image: faker.image.avatar(),
-});
-const createUsers = (numUsers = 5) => new Array(numUsers).fill(undefined).map(createUser);
+// import styled from "styled-components";
+// import Button from "../shared/Button";
+// import faker from "faker";
+// const createUser = () => ({
+//   id: faker.datatype.uuid(),
+//   name: faker.name.findName(),
+//   email: faker.internet.email(),
+//   address: faker.address.streetAddress(),
+//   bio: faker.lorem.sentence(),
+//   image: faker.image.avatar(),
+// });
+// const createUsers = (numUsers = 5) => new Array(numUsers).fill(undefined).map(createUser);
 
-const fakeUsers = createUsers(2000);
+// const fakeUsers = createUsers(2000);
 
-const TextField = styled.input`
-  height: 32px;
-  width: 200px;
-  border-radius: 3px;
-  border-top-left-radius: 5px;
-  border-bottom-left-radius: 5px;
-  border-top-right-radius: 0;
-  border-bottom-right-radius: 0;
-  border: 1px solid #e5e5e5;
-  padding: 0 32px 0 16px;
+// const TextField = styled.input`
+//   height: 32px;
+//   width: 200px;
+//   border-radius: 3px;
+//   border-top-left-radius: 5px;
+//   border-bottom-left-radius: 5px;
+//   border-top-right-radius: 0;
+//   border-bottom-right-radius: 0;
+//   border: 1px solid #e5e5e5;
+//   padding: 0 32px 0 16px;
 
-  &:hover {
-    cursor: pointer;
-  }
-`;
+//   &:hover {
+//     cursor: pointer;
+//   }
+// `;
 
-const ClearButton = styled(Button)`
-  border-top-left-radius: 0;
-  border-bottom-left-radius: 0;
-  border-top-right-radius: 5px;
-  border-bottom-right-radius: 5px;
-  height: 34px;
-  width: 32px;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+// const ClearButton = styled(Button)`
+//   border-top-left-radius: 0;
+//   border-bottom-left-radius: 0;
+//   border-top-right-radius: 5px;
+//   border-bottom-right-radius: 5px;
+//   height: 34px;
+//   width: 32px;
+//   text-align: center;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+// `;
 
-const FilterComponent = ({ filterText, onFilter, onClear }) => (
-  <>
-    <TextField
-      id="search"
-      type="text"
-      placeholder="Filter By Name"
-      aria-label="Search Input"
-      value={filterText}
-      onChange={onFilter}
-    />
-    <ClearButton type="button" onClick={onClear}>
-      X
-    </ClearButton>
-  </>
-);
+// const FilterComponent = ({ filterText, onFilter, onClear }) => (
+//   <>
+//     <TextField
+//       id="search"
+//       type="text"
+//       placeholder="Filter By Name"
+//       aria-label="Search Input"
+//       value={filterText}
+//       onChange={onFilter}
+//     />
+//     <ClearButton type="button" onClick={onClear}>
+//       X
+//     </ClearButton>
+//   </>
+// );
 
-const [filterText, setFilterText] = React.useState("");
-const [resetPaginationToggle, setResetPaginationToggle] = React.useState(false);
-const filteredItems = fakeUsers.filter(
-  (item) => item.name && item.name.toLowerCase().includes(filterText.toLowerCase())
-);
+// const [filterText, setFilterText] = React.useState("");
+// const [resetPaginationToggle, setResetPaginationToggle] = React.useState(false);
+// const filteredItems = fakeUsers.filter(
+//   (item) => item.name && item.name.toLowerCase().includes(filterText.toLowerCase())
+// );
 
-const subHeaderComponentMemo = React.useMemo(() => {
-  const handleClear = () => {
-    if (filterText) {
-      setResetPaginationToggle(!resetPaginationToggle);
-      setFilterText("");
-    }
-  };
+// const subHeaderComponentMemo = React.useMemo(() => {
+//   const handleClear = () => {
+//     if (filterText) {
+//       setResetPaginationToggle(!resetPaginationToggle);
+//       setFilterText("");
+//     }
+//   };
 
-  return (
-    <FilterComponent onFilter={(e) => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} />
-  );
-}, [filterText, resetPaginationToggle]);
+//   return (
+//     <FilterComponent onFilter={(e) => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} />
+//   );
+// }, [filterText, resetPaginationToggle]);
+
 const columns = [
   {
     name: "First Name",
@@ -493,16 +494,16 @@ const MyComponent = () => {
   return (
     <DataTable
       columns={columns}
-      data={filteredItems}
+      data={data}
       pagination
       defaultSortFieldId={1}
       subHeader
       persistTableHead
       highlightOnHover
       pointerOnHover
-      subHeaderComponent={subHeaderComponentMemo}
-      selectableRows
-      paginationResetDefaultPage={resetPaginationToggle}
+      // subHeaderComponent={subHeaderComponentMemo}
+      // selectableRows
+      // paginationResetDefaultPage={resetPaginationToggle}
     />
   );
 };
